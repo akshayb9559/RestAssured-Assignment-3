1f6c42e8919073115626af4897a4048a8602d436
Akshay Bramhane <44193607+akshayb9559@users.noreply.github.com>
2020-10-25 17:40:29 +0530

    Add files via upload

---
+++ RestAssured_ResponseAssertion/src/test/java/stepDefinitions/ResponseStep.java
@@ -0,0 +1,39 @@
+package stepDefinitions;
+
+import org.junit.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class ResponseStep {
+
+	Response resp;
+
+	@Given("API for foreign exchange")
+	public void api_for_foreign_exchange() {
+		RestAssured.baseURI = "https://api.ratesapi.io";
+	}
+
+	@When("posted with correct information")
+	public void posted_with_correct_information() {
+		resp = RestAssured.get("/api/latest");
+	}
+
+	@Then("validate positive response code received")
+	public void validate_positive_response_code_received() {
+		System.out.println("Status Code is: " + resp.getStatusCode());
+		System.out.println("Status Line is: "+resp.getStatusLine());
+		
+		int statusCode = resp.getStatusCode();
+		Assert.assertEquals(200, statusCode);
+		
+		Assert.assertTrue(statusCode==200);
+		Assert.assertFalse(statusCode!=200);
+		
+		String statusLine = resp.getStatusLine();
+		Assert.assertEquals("HTTP/1.1 200 OK", statusLine);		
+	}
+}
---
+++ RestAssured_ResponseAssertion/src/test/resources/Features/response.feature
@@ -0,0 +1,6 @@
+Feature: Validate response code using assertions
+
+  Scenario: Add assertions for response code
+    Given API for foreign exchange
+    When posted with correct information
+    Then validate positive response code received
